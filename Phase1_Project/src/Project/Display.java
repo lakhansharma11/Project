2cee1fce2770c420540ddefcdfb3427af06bfa81
lakhansharma081 <lakhansharma081@ip-172-31-76-222.ec2.internal>
2021-01-16 14:38:43 +0000

    new changes for display all items

--- Phase1_Project/src/Project/Display.java
+++ Phase1_Project/src/Project/Display.java
@@ -1,5 +1,7 @@
 package Project;
 
+import java.util.Scanner;
+
 public class Display {
 
 	public static void main(String[] args) {
@@ -7,10 +9,36 @@ public class Display {
 		System.out.println("I am a full stack Java Developer.");
 		System.out.println("On this application, You can create a file, delete a file of search a file form Directory");
 		System.out.println();
+		
 		System.out.println("1 Display all the File and Directory");
 		System.out.println("2 Create a new file");
 		System.out.println("3 Delete a file");
 		System.out.println("4 Search a file");
+		Scanner scanner = new Scanner( System.in );
+		 System.out.println("Enter the  number");
+		 int select = scanner.nextInt();
+		/// = outputMenu();
+		
+		//String option;
+		//select = 1;
+		switch(select)
+		{
+		case 1 :
+			
+			System.out.println("first option");
+			LockedMe.DisplayItem();
+			
+			
+		
+		break;
+		
+		case 2:
+			
+			System.out.println("f2nd option");
+			break;
+		
+		}
+		
 	}
 
 }
--- Phase1_Project/src/Project/LockedMe.java
+++ Phase1_Project/src/Project/LockedMe.java
@@ -1,5 +1,43 @@
 package Project;
 
+import java.io.*;
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class LockedMe {
+	
+	
+	
+
+	
+
+	public static void DisplayItem()
+	{
+		//String file;
+		//try
+		
+		Scanner scanner = new Scanner( System.in );
+		 System.out.println("Enter the file path:");
+		 String dirPath = scanner.nextLine();
+		 File folder = new File(dirPath);
+		
+		
+			File[] fileList = folder.listFiles();
+			Arrays.sort(fileList);
+			System.out.println("\nTotal number of items present in the directory:" + fileList.length);
+				
+			for(File file : fileList)
+			
+		 {
+			
+			System.out.println(file.getName());
+		 }
+
+			//catch(Execption e)
+//		{
+//		System.out.println("error");
+//		}
+	
+	}
 
 }
