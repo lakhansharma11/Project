41b32c02513a12a3543f1f64ff6ce03f8f741791
lakhansharma081 <lakhansharma081@ip-172-31-76-222.ec2.internal>
2021-01-16 04:46:56 +0000

    Project phase 1

---
+++ Phase1_Project/src/Project/Display.java
@@ -0,0 +1,16 @@
+package Project;
+
+public class Display {
+
+	public static void main(String[] args) {
+		System.out.println("Hello my name is Lakhan Sharma.");
+		System.out.println("I am a full stack Java Developer.");
+		System.out.println("On this application, You can create a file, delete a file of search a file form Directory");
+		System.out.println();
+		System.out.println("1 Display all the File and Directory");
+		System.out.println("2 Create a new file");
+		System.out.println("3 Delete a file");
+		System.out.println("4 Search a file");
+	}
+
+}
